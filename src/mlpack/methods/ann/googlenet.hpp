9bd955d323eb8e2bf6d424fd9f54bf5ea63fca18
nilayjain <nilayjain13@gmail.com>
2016-08-18 08:27:24 +0000

    removed errors

--- src/mlpack/methods/ann/googlenet.hpp
+++ src/mlpack/methods/ann/googlenet.hpp
@@ -37,8 +37,8 @@ namespace ann /** Artificial Neural Network. */ {
 class GoogleNet
 {
  public:
-  ConvLayer<> conv1, conv2, conv3, conv4, conv5;
-  BaseLayer2D<RectifierFunction> base1, base2, base3, base4, base5;
+  ConvLayer<> conv1, conv2, conv3, conv4;
+  BaseLayer2D<RectifierFunction> base1, base2, base3, base4;
   BaseLayer<IdentityFunction, arma::cube, arma::cube> id1, id2;
   InceptionLayer<> inception3a, inception3b, inception4a, inception4b,
       inception4c, inception4d, inception4e, inception5a, inception5b;
@@ -47,12 +47,12 @@ class GoogleNet
   SoftmaxLayer<> softmax1, softmax2, softmax3;
   PoolingLayer<MaxPooling> pool1, pool2, pool3, pool4;
   PoolingLayer<MeanPooling> pool5, pool6, pool7;
-  ConnectLayer connect1, connect2;
+  //ConnectLayer connect1, connect2;
   OneHotLayer output1, output2, output3, output4, output5;
   GoogleNet() :
     conv1(3, 64, 7, 7, 2, 2, 3, 3),
     pool1(3, 2),
-    conv3(64, 192, 3, 3, 1, 1, 1, 1),
+    conv2(64, 192, 3, 3, 1, 1, 1, 1),
     pool2(3, 2),
     inception3a(192, 64, 96, 128, 16, 32, 32),
     inception3b(256, 128, 128, 192, 32, 96, 64),
@@ -69,18 +69,18 @@ class GoogleNet
     drop1(0.4),
     linear1(1024, 1000),
     pool6(5, 3),
-    conv4(512, 128, 1, 1, 1, 1, 1, 1),
+    conv3(512, 128, 1, 1, 1, 1, 1, 1),
     linear2(4 * 4 * 128, 1024),
     drop2(0.7),
     linear3(1024, 1000),
     pool7(5, 3),
-    conv5(528, 128, 1, 1, 1, 1, 1, 1),
+    conv4(528, 128, 1, 1, 1, 1, 1, 1),
     linear4(4 * 4 * 128, 1024),
     drop3(0.7),
     linear5(1024, 1000)
   {
 
-    auto aux2 = std::tie(pool7, conv5, linear4, drop3, linear5, softmax3);
+    auto aux2 = std::tie(pool7, conv4, base4, linear4, drop3, linear5, softmax3);
     CNN<decltype(aux2), decltype(output3),
         RandomInitialization, MeanSquaredErrorFunction> auxNet2(aux2, output3);
 
@@ -89,9 +89,9 @@ class GoogleNet
     CNN<decltype(main3), decltype(output1),
         RandomInitialization, MeanSquaredErrorFunction> mainNet3(main3, output1);
 
-    connect2 = ConnectLayer<decltype(mainNet3), decltype(auxNet2)>(mainNet3, auxNet2);
+    auto connect2 = ConnectLayer<decltype(mainNet3), decltype(auxNet2)>(mainNet3, auxNet2);
     
-    auto aux1 = std::tie(pool6, conv4, linear2, drop2, linear3, softmax2);
+    auto aux1 = std::tie(pool6, conv3, base3, linear2, drop2, linear3, softmax2);
     CNN<decltype(aux1), decltype(output2),
         RandomInitialization, MeanSquaredErrorFunction> auxNet1(aux1, output2);
 
@@ -99,9 +99,9 @@ class GoogleNet
     CNN<decltype(main2), decltype(output4),
         RandomInitialization, MeanSquaredErrorFunction> mainNet2(main2, output4);    
 
-    connect1 = ConnectLayer<decltype(mainNet2), decltype(auxNet1)>(mainNet2, auxNet1);
+    auto connect1 = ConnectLayer<decltype(mainNet2), decltype(auxNet1)>(mainNet2, auxNet1);
 
-    auto main1 = std::tie(conv1, pool1, conv3, pool2, inception3a, inception3b, 
+    auto main1 = std::tie(conv1, pool1, conv2, base2, pool2, inception3a, inception3b, 
                           pool3, inception4a, connect1);
     CNN<decltype(main1), decltype(output5),
         RandomInitialization, MeanSquaredErrorFunction> mainNet1(main1, output5);
